1e07eaafb5d53a5a83de62e1090233824e07fcb9
divyanshu <jhanwar.divyanshu@gmail.com>
2023-12-05 22:26:21 +0530

    Text Extraction from Image completed successfully

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,14 +1,21 @@
-# This library is used for optical character recognition (ocr) to extract text from images.
 import pytesseract
-from PIL import Image   # This library is used to open and manipulate images
+from PIL import Image
 import os
 
 class OCREngine:
     @staticmethod
     def extract_text(file_path):
         try:
-            image = Image.open(file_path)    # open image file
-            text = pytesseract.image_to_string(image)   # extract text from image
-            return text     # The extracted text is returned as a result
-        except Exception as e: 
-           return str(e)
+            # Set the path to the Tesseract executable
+            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+            # Open image file
+            image = Image.open(file_path)
+
+            # Extract text from the image
+            text = pytesseract.image_to_string(image)
+
+            # Return the extracted text as a result
+            return text
+        except Exception as e:
+            return str(e)
