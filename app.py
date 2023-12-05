ef508d686fcc612440672602040e76a9a656990b
divyanshu <jhanwar.divyanshu@gmail.com>
2023-12-05 16:40:39 +0530

    python app.py file tested

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from ocr_engine import OCREngine
-from utils.helper import allowed_file, save_uploaded_file
+from helper import allowed_file, save_uploaded_file
 import os
 
 app = Flask(__name__)
